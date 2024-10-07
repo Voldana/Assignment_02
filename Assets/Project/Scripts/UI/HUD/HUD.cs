3919d0ff461db59b65e80380383e483e0bd1eeba
Mohammad Darandeh <m.darande@gmail.com>
2024-10-07 15:28:08 -0400

    Add timer and remaining moves text to the HUD

--- Assets/Project/Scripts/UI/HUD/HUD.cs
+++ Assets/Project/Scripts/UI/HUD/HUD.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Collections.Generic;
 using Project.Scripts.Game;
+using TMPro;
 using UnityEngine;
 using Zenject;
 
@@ -9,13 +8,24 @@ namespace Project.Scripts.UI.HUD
     public class HUD : MonoBehaviour
     {
         [SerializeField] private Transform objectives;
-        
+        [SerializeField] private TMP_Text clock;
+        [SerializeField] private TMP_Text moves;
+
         [Inject] private Objective.Factory factory;
         [Inject] private LevelSetting levelSetting;
 
+        private Timer timer;
+
         private void Start()
         {
+            SetRemainingMoves();
             CreateObjectives();
+            SetTimer();
+        }
+
+        private void SetRemainingMoves()
+        {
+            moves.text = levelSetting.moves.ToString();
         }
 
         private void CreateObjectives()
@@ -26,7 +36,18 @@ namespace Project.Scripts.UI.HUD
 
         private void SetTimer()
         {
-            
+            UpdateClock(levelSetting.time * 60);
+            timer = new Timer(levelSetting.time * 60, UpdateClock, OnTimerEnd);
+            timer.Start();
+        }
+
+        private void UpdateClock(int newTime)
+        {
+            clock.text = $"{newTime / 60}:{newTime % 60}";
+        }
+
+        private void OnTimerEnd()
+        {
         }
     }
 }
--- Assets/Project/Scripts/UI/HUD/Timer.cs
+++ Assets/Project/Scripts/UI/HUD/Timer.cs
@@ -1,7 +1,82 @@
+using System;
+using DG.Tweening;
+
 namespace Project.Scripts.UI.HUD
 {
-    public class Timer
+    internal class Timer
     {
-        
+        private readonly int startTime;
+        private Action<int> onTick;
+        private Action onComplete;
+
+        private bool timerStarted;
+        private bool stopTimer;
+        private int timer;
+        private Tween tween;
+
+        public Timer(int startTime, Action<int> onTick, Action onComplete)
+        {
+            this.startTime = startTime;
+            this.onTick = onTick;
+            this.onComplete = onComplete;
+        }
+
+        public void Start()
+        {
+            if (timerStarted)
+                return;
+            tween?.Kill();
+
+            timerStarted = true;
+            stopTimer = false;
+            timer = startTime;
+            PassServerTime();
+        }
+
+        public void Reset()
+        {
+            timerStarted = false;
+            Start();
+        }
+
+        private void PassServerTime()
+        {
+            tween = DOVirtual.DelayedCall(1, () =>
+            {
+                timer--;
+                if (stopTimer) return;
+                onTick?.Invoke(timer);
+                if (timer <= 0)
+                {
+                    onComplete?.Invoke();
+                    timerStarted = false;
+                    return;
+                }
+
+                PassServerTime();
+            });
+        }
+
+        public bool IsStarted()
+        {
+            return timerStarted;
+        }
+
+        public void IncreaseTime(int amount)
+        {
+            timer += amount;
+        }
+
+        public int GetRemainingTime()
+        {
+            return timer;
+        }
+
+        public void Stop()
+        {
+            timerStarted = false;
+            stopTimer = true;
+            tween?.Kill();
+        }
     }
 }
