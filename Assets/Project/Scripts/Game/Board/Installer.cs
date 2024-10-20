8af7a2c1bceae74856cf1802f0652bb62e5e4466
Mohammad Darandeh <m.darande@gmail.com>
2024-10-20 13:22:55 -0400

    Remove redundant scripts

--- Assets/Project/Scripts/Game/Board/Installer.cs
+++ Assets/Project/Scripts/Game/Board/Installer.cs
@@ -5,13 +5,11 @@ namespace Project.Scripts.Game.Board
 {
     public class Installer : MonoInstaller
     {
-        // [SerializeField] private Cell cell;
         [SerializeField] private Gem gem;
 
         public override void InstallBindings()
         {
             Container.BindFactory<GemType ,Gem, Gem.Factory>().FromComponentInNewPrefab(gem).AsSingle();
-            // Container.BindFactory<Transform,Gem, Cell, Cell.Factory>().FromComponentInNewPrefab(cell).AsSingle();
         }
     }
 }
