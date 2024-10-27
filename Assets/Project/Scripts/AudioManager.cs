0fc04e4eab86d1030f3b91190f4e08d0767b7ee6
Mohammad Darandeh <m.darande@gmail.com>
2024-10-27 14:17:09 -0400

    Remove unused sound effects

--- Assets/Project/Scripts/AudioManager.cs
+++ Assets/Project/Scripts/AudioManager.cs
@@ -6,19 +6,15 @@ namespace Project.Scripts
     {
         [SerializeField] private AudioSource audioSource;
         [SerializeField] private AudioClip click;
-        [SerializeField] private AudioClip deselect;
         [SerializeField] private AudioClip win;
         [SerializeField] private AudioClip lose;
-        [SerializeField] private AudioClip woosh;
         [SerializeField] private AudioClip pop;
         [SerializeField] private AudioClip failed;
 
 
         public void PlayClick() => audioSource.PlayOneShot(click);
-        public void PlayDeselect() => audioSource.PlayOneShot(deselect);
         public void PlayWin() => audioSource.PlayOneShot(win);
         public void PlayLose() => audioSource.PlayOneShot(lose);
-        public void PlayWoosh() => PlayRandomPitch(woosh);
         public void PlayPop() => PlayRandomPitch(pop);
 
         public void PlayFailed() => PlayRandomPitch(failed);
